0def699420fa4788db446ab5a362f622f9e099d3
Kristine Domingo <kristine@cs.utexas.edu>
2016-03-12 14:03:47 -0600

    Moved static folder into app folder

--- idb.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__, static_url_path='')
-
-@app.route('/')
-def splash():
-    return app.send_static_file('index.html')
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
