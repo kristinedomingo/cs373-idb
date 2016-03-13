1ac57a326d2f87ad8559944c47b173bc31d1b762
Kristine Domingo <kristine@cs.utexas.edu>
2016-03-13 11:55:24 -0500

    Added flask route for about, changed links to routes (vs html pages)

--- app/idb.py
+++ app/idb.py
@@ -5,5 +5,9 @@ app = Flask(__name__, static_url_path='')
 def splash():
     return app.send_static_file('index.html')
 
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
