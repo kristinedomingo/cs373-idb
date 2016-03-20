89c6e0268b19d9a176f400b06b4478ad2a54ad80
Xavier Ramirez <alpha@Xaviers-MacBook-Pro.local>
2016-03-19 21:11:24 -0500

    Beautified html and CSS. Converted tabs to spaces in all JS files! Fixes #45

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -2,40 +2,40 @@
 
 /* App Module */
 var sweetMusicApp = angular.module('sweetMusicApp', [
-	'controllers',
-	'services',
-	'ngRoute',
-	'door3.css'
+    'controllers',
+    'services',
+    'ngRoute',
+    'door3.css'
 ]);
 
 sweetMusicApp.config(['$routeProvider',
-	function($routeProvider){
-		console.log("trying to route");
-		console.log($routeProvider);
-		$routeProvider.
-			when('/', {
-				templateUrl : '/partials/splash.html',
-				controller : 'SplashCtrl',
-				css : 'css/splash.css'
-			}).
-			when('/artists', {
-				templateUrl : '/partials/artists.html',
-				controller : 'ArtistTableCtrl',
-				css : 'css/artists.css'
-			}).
-			when('/test/:artistID', {
-				templateUrl : '/partials/artist-details.html',
-				controller : 'ArtistDetailsCtrl'
-			}).
-			when('/about', {
-				templateUrl : 'partials/about.html',
-				controller : 'AboutCtrl',
-				css : 'css/about.css'
-			}).
-			otherwise({
-        		redirectTo: '/'
-      		});
-		console.log("trying to configure");
+    function($routeProvider){
+        console.log("trying to route");
+        console.log($routeProvider);
+        $routeProvider.
+            when('/', {
+                templateUrl : '/partials/splash.html',
+                controller : 'SplashCtrl',
+                css : 'css/splash.css'
+            }).
+            when('/artists', {
+                templateUrl : '/partials/artists.html',
+                controller : 'ArtistTableCtrl',
+                css : 'css/artists.css'
+            }).
+            when('/test/:artistID', {
+                templateUrl : '/partials/artist-details.html',
+                controller : 'ArtistDetailsCtrl'
+            }).
+            when('/about', {
+                templateUrl : 'partials/about.html',
+                controller : 'AboutCtrl',
+                css : 'css/about.css'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+        console.log("trying to configure");
 }]);
 
 console.log("App loaded");
--- app/static/js/services.js
+++ app/static/js/services.js
@@ -2,33 +2,33 @@
 
 /* Services */
 angular.module('services',[])
-	.factory('artistService', function($http){
-		return {
-			getArtists: function(){
-				//since $http.get returns a promise,
-		        //and promise.then() also returns a promise
-		        //that resolves to whatever value is returned in it's 
-		        //callback argument, we can return that.
-		        return $http.get('/get_artists').then(function(result){
-		        	return result.data;
-		        });
-	    	}
-	    }})
-	.factory('persistArtist', function(){
-		//persist Artist data on routing so that we 
-		//don't have to do another Http request
-		var savedArtists = {};
-		function saveArtists(data){
-			savedArtists = data;
-		}
+    .factory('artistService', function($http){
+        return {
+            getArtists: function(){
+                //since $http.get returns a promise,
+                //and promise.then() also returns a promise
+                //that resolves to whatever value is returned in it's 
+                //callback argument, we can return that.
+                return $http.get('/get_artists').then(function(result){
+                    return result.data;
+                });
+            }
+        }})
+    .factory('persistArtist', function(){
+        //persist Artist data on routing so that we 
+        //don't have to do another Http request
+        var savedArtists = {};
+        function saveArtists(data){
+            savedArtists = data;
+        }
 
-		function getArtists(){
-			return savedArtists;
-		}
+        function getArtists(){
+            return savedArtists;
+        }
 
-		return {
-			set:saveArtists,
-			get:getArtists
-		}
-	});
+        return {
+            set:saveArtists,
+            get:getArtists
+        }
+    });
 
