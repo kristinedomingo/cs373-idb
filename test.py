811c371df3fa2cbc71047e2d4649dbb8dfc016e1
Jorge Munoz <jorgmuno@cs.utexas.edu>
2016-03-24 15:45:11 -0500

    added the last test module for tracks

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ class TestArtist(TestCase):
 
     def test_get_all_artist(self):
         artists = Artist.query.all()
-        assert len(Artist) == 2
+        assert len(artists) == 2
 
     def test_filtering_artist(self):   
         artists = Artist.query.filter_by(category == 'Lullaby').first()
@@ -71,7 +71,7 @@ class TestAlbum(TestCase):
         assert album.artist_name == 'Chet Faker' && album.name == '1998 Melbou Edition'   
 
     def test_add_delete_album(self):
-        artists = Album( "Skrillex and Diplo present Jack U", 2015-02-24, 2125000, 10)
+        album = Album( "Skrillex and Diplo present Jack U", 2015-02-24, 2125000, 10)
         db.session.add()
         db.session.commit()
         assert len(Album.query.all()) == 2
@@ -79,6 +79,48 @@ class TestAlbum(TestCase):
         Album.query.filter_by(length < 2125000).delete()
         db.session.commit()
         assert len(Album.query.all()) == 1 
+
+
+class TestTrack(TestCase):
+
+    def create_app(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = TEST_DATABASE_URI
+        return app
+
+    def setUp(self):
+        db.create_all()
+        track = Track("Drop the Game", "Flume, Chet Faker",2013-11-12, "Lockjaw", 1923800, )
+        track2 = Track("Landed on Mars", "Atlas Bound", 2015-03-18, "Landed on Mars",224000)
+        db.session.add(track)
+        db.session.add(track2)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_get_all_tracks(self):
+        tracks = Track.query.all()
+        assert len(tracks) == 2
+
+    def test_filtering_tracks(self):   
+        track = Track.query.filter_by(name == 'Landed on Mars').first()
+        assert track.artist_name == "Atlas Bound" && track.album  == "Landed on Mars"
+
+        track = Track.query.filter_by(duration > 300000).first()
+        assert track.artist_name == 'Flume, Chet Faker' && track.name == 'Drop the Game'   
+
+    def test_add_delete_tracks(self):
+        track = Track("Where are U Now", "Jack U ,Skrillex, Diplo, Justin Bieber", 2015-02-24, "Skrillex and Diplo present Jack U",2015-02-24, 250000)
+        db.session.add()
+        db.session.commit()
+        assert len(Track.query.all()) == 2
+
+        Tracks.query.filter_by(duration == 250000).delete()
+        db.session.commit()
+        assert len(Track.query.all()) == 1 
+
+
 
 
 if __name__ == '__main__':
