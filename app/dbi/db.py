7c34141f99abfc21f4f3bdcab5e5f61abbbcd820
Jorge Munoz <jorgmuno@cs.utexas.edu>
2016-03-30 17:21:53 -0500

    added the cache for my spotify key

--- app/dbi/db.py
+++ app/dbi/db.py
@@ -15,7 +15,8 @@ SQLALCHEMY_DATABASE_URI = \
 
 app = Flask(__name__, static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@127.0.0.1/sweetmusic'
+#if you want to run it locally if you have mySQL make sure you created a schema called sweetmusic or change it to a different database
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@127.0.0.1/sweetmusic'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 manager = Manager(app)
