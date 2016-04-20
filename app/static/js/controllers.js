d6b400ffcce8d22d566bd560c8030a9120ffb0dd
Kristine Domingo <kristine@cs.utexas.edu>
2016-04-20 16:26:14 -0500

    Made enter key work with both search bars

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -6,7 +6,10 @@ angular.module('controllers', ['ui.bootstrap', 'chart.js'])
 /**
  * Splash Page Controller
  */
-.controller('SplashCtrl', ['$scope', function($scope) {
+.controller('SplashCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.search = function() {
+        $location.path("/results/all/" + $scope.searchTerm);
+    }
 }])
 
 /**
@@ -381,7 +384,7 @@ angular.module('controllers', ['ui.bootstrap', 'chart.js'])
  * Navigation Controller
  * Underlines the link to the page the user is currently on.
  */
-.controller('NavCtrl', ['$scope', '$location', function($scope, $location){
+.controller('NavCtrl', ['$scope', '$location', function($scope, $location) {
     $scope.isActive = function(viewLocation) {
         return $location.path().indexOf(viewLocation) == 0;
     }
@@ -391,7 +394,12 @@ angular.module('controllers', ['ui.bootstrap', 'chart.js'])
         return !($location.path() == "/");
       }
     });
+
     $scope.isSplash = function(){
         return !($location.path() == "/");
+    }
+
+    $scope.search = function() {
+        $location.path("/results/all/" + $scope.searchTerm);
     }
 }]);
